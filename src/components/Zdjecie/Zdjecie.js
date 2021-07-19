e5f608000ad3ffa12078485137479357a6acd22a
AdamDrozapol <73120798+AdamDrozapol@users.noreply.github.com>
2021-07-19 12:40:27 +0200

    Dodano hover z likes and downloads

--- src/components/Zdjecie/Zdjecie.js
+++ src/components/Zdjecie/Zdjecie.js
@@ -2,7 +2,7 @@ import './Zdjecie.css'
 import { useParams } from 'react-router-dom'
 import {useState, useEffect} from 'react'
 import apiPhoto from '../../api/apiPhoto'
-
+import styl from 'styled-components'
 function Zdjecie()
 {
     const { id } = useParams()
@@ -16,9 +16,40 @@ function Zdjecie()
             img(data)}  </div>
 }
 
-function img({alt_description, urls})
+function img({alt_description, urls, likes,downloads, location})
 {
-return <div><h1>{alt_description}</h1> <img src={urls.full} /></div>
+return <div><StylH1><h1>{alt_description}</h1></StylH1>
+<img src={urls.full} />
+
+<StyledZdjecia><h2>Polubień: {likes}<br/>Pobrań: {downloads}<br/> Kraj: {location?.country} </h2></StyledZdjecia>
+
+</div>
+    
+
 }
+
+const StylH1 = styl.h1
+`
+color:white;
+text-align: center;
+`
+
+const StyledZdjecia = 
+
+styl.h2
+`
+position: absolute;
+top: 2em;
+left: 0;
+width: 100%;
+height: 100%;
+color:transparent;
+text-align: center;
+
+  &:hover 
+  {
+    color: black;
+  }
+`
 
 export default Zdjecie
